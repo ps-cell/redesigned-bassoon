f9b0ffc10e26671c7e6a6ca328698613b13ff2a9
ndeschultz <ndeschultz@googlemail.com>
2022-01-29 03:33:26 +0100

    initialized ui

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # redesigned-bassoon
 ## javascript-based decentralized repository for 3D models with [gun.js](https://github.com/amark/gun) 
+
+### svelte-links
+- https://svelte.dev/blog/the-easiest-way-to-get-started
+- https://sveltematerialui.com/?ref=madewithsvelte.com
+- https://sveltematerialui.com/INSTALL.md
---
+++ ui/src/App.svelte
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="./node_modules/svelte-material-ui/bare.css" />
+
+<script lang="ts">
+	import Card from '@smui/card'
+	import Button from '@smui/button'
+	let clicked = 0;
+</script>
+
+<main>
+	<Card padded>
+		<h1>The Dude abides.</h1>
+		<Button color="#ff3e00" on:click={() => clicked++} ripple={true}>
+    		Praise
+  		</Button>
+		<p>{clicked}</p>
+	</Card>
+</main>
