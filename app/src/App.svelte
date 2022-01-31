8df2b6688c4edf463f6d7346dd7b6dc12eba150a
aroefke <anton.roefke@outlook.de>
2022-01-31 02:33:16 +0100

    new file structure

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,35 +1,10 @@
 <link rel="stylesheet" href="./node_modules/svelte-material-ui/bare.css" />
 
 <script lang="ts">
-	import GUN from "gun"
-	import Card from '@smui/card'
-	import Button from '@smui/button'
-
-	// connect to relay (must be running, see readme)
-	export const gun = Gun(["http://localhost:8765/gun"])
-	let count = gun.get("count", (ack) => {})
-	let _clicked = count.val || 0
-
-	// listen for changes on count
-	count.on((data) => {
-		console.log("Update: ", data)
-		_clicked = data.val
-	})
-
-	// click-event handler
-	async function click() {
-		_clicked++
-		count.put({"val": _clicked})
-	}
+	import DudeCard from "./dude-praise.svelte"
 
 </script>
 
 <main>
-	<Card padded>
-		<h1>The Dude abides.</h1>
-		<Button color="#ff3e00" on:click={click} ripple >
-    		Praise
-		</Button>
-		<p>{_clicked}</p>
-	</Card>
+	<DudeCard/>
 </main>
---
+++ app/src/dude-praise.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import GUN from "gun"
+	import Card from '@smui/card'
+	import Button from '@smui/button'
+
+	// connect to relay (must be running, see readme)
+	export const gun = Gun(["http://localhost:8765/gun"])
+	let count = gun.get("count", (ack) => {})
+	let _clicked = count.val || 0
+
+	// listen for changes on count
+	count.on((data) => {
+		console.log("Update: ", data)
+		_clicked = data.val
+	})
+
+	// click-event handler
+	async function click() {
+		_clicked++
+		count.put({"val": _clicked})
+	}
+
+</script>
+
+<Card padded>
+	<h1>The Dude abides.</h1>
+	<Button color="#ff3e00" on:click={click} ripple >
+		Praise
+	</Button>
+	<p>{_clicked}</p>
+</Card>
