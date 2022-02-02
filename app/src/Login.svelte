c100e9ec850e5c8315f9b4463bafc10fec7a49ec
ps-cell <seidelp@protonmail.com>
2022-02-02 23:34:52 +0100

    login-card design improvements

--- app/src/Login.svelte
+++ app/src/Login.svelte
@@ -1,7 +1,9 @@
-<script>
+<script lang="ts">
   import Card from '@smui/card'
 	import Button from '@smui/button'
   import Textfield from '@smui/textfield'
+  import MenuSurface from '@smui/menu-surface'
+  import LayoutGrid, { Cell } from '@smui/layout-grid'
   import { user, username, loggedIn } from './user'
 
   let _username = $username
@@ -26,35 +28,70 @@
       }
     })
   }
+
+  function keydownHandler(event: CustomEvent | KeyboardEvent) {
+    event = event as KeyboardEvent
+    if (event.key === "Enter") {
+      login()
+    }
+  }
 </script>
 
+<style>
+</style>
 
-<Card padded style="margin: 0px 500px 0px 500px;">
+
+<Card padded style="margin: auto;">
+  <LayoutGrid 
+  >
 
 {#if !$loggedIn}
-  <Textfield
-    label="username"
-    type="email"
-    bind:value={_username}
-    input$emptyValueUndefined
-  >
-  </Textfield>
+  <Cell
+    spanDevices={{ desktop: 6, tablet: 12, phone: 12 }}
+    >
+      <Textfield
+        label="username"
+        type="email"
+        bind:value={_username}
+        on:keydown={keydownHandler}
+        input$emptyValueUndefined
+      >
+      </Textfield>
+  </Cell>
 
-  <Textfield
-    label="password"
-    type="password"
-    bind:value={_password}
-    input$emptyValueUndefined
-  >
-  </Textfield>
+  <Cell
+    spanDevices={{ desktop: 6, tablet: 12, phone: 12 }}
+    >
+      <Textfield
+        label="password"
+        type="password"
+        bind:value={_password}
+        on:keydown={keydownHandler}
+        input$emptyValueUndefined
+      >
+      </Textfield>
+  </Cell>
 
-  <Button on:click={login}>Login</Button>
-  <Button on:click={signup}>Signup</Button>
+  <Cell
+    spanDevices={{ desktop: 6, tablet: 12, phone: 12 }}
+    >
+      <Button on:click={login}>Login</Button>
+  </Cell>
+  <Cell
+    >
+      <Button on:click={signup}>Signup</Button>
+  </Cell>
 {/if}
 
 {#if $loggedIn}
-  <h1>{$username}</h1>
-  <Button on:click={logout}>Logout</Button>
+  <Cell>
+    <h1>{$username}</h1>
+  </Cell>
+  <Cell style="display: flex; justify-content: center; flex-direction: column;">
+    <Button on:click={logout}>Logout</Button>
+  </Cell>
 {/if}
+
+  </LayoutGrid>
 
 </Card>
